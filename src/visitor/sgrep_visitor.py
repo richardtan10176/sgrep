e6bdb958ccc8d7664c1792070b66b81574bad893
Richard Tan <richardtan787@gmail.com>
2025-12-16 23:04:47 -0500

    structure proj

---
+++ src/visitor/sgrep_visitor.py
@@ -0,0 +1,32 @@
+import ast
+
+class sgrepVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.scope_stack = [] #tracks the current path (e.g ['Database', 'connect'])
+        self.chunks = [] #stores the results
+
+    def _get_context_header(self):
+        return " > ".join(self.scope_stack)
+
+    def visit_ClassDef(self, node):
+        self.scope_stack.append(f"Class: {node.name}")
+        self.generic_visit(node)
+        self.scope_stack.pop()
+
+    def visit_FunctionDef(self, node):
+        self.scope_stack.append(f"Function: {node.name}")
+        func_source = ast.get_source_segment(self.source_code, node)
+        header = self._get_context_header()
+        self.chunks.append({
+            "context": header,
+            "code": func_source,
+            "line_start": node.lineno
+        })
+        self.generic_visit(node)
+        self.scope_stack.pop()
+
+    def parse(self, source_code):
+        self.source_code = source_code
+        tree = ast.parse(source_code)
+        self.visit(tree)
+        return self.chunks
